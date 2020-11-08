5e36c3e569f5e729d39fdf4b0a84f8f2321ae0e9
LavrovSergey <54863977+LavrovSergey@users.noreply.github.com>
2020-11-08 02:08:08 +0200

    Update ConsoleApplication1etrgerge.cpp

--- ConsoleApplication1etrgerge.cpp
+++ ConsoleApplication1etrgerge.cpp
@@ -5,12 +5,32 @@
 #include <conio.h>
 #include "windows.h"
 using namespace std;
+template<class T, class U> class BinarySearchTree;
+template< class T, class U>
+class Node {
+	friend class BinarySearchTree<T, Node<T, U>>;
+public:
+	Node<T, U>* left=NULL;
+	Node<T, U>* right = NULL;
+	U* data;
+	/*std::vector<T> v;
+	int id = 0;
+	bool way;*/
+};
+template<class T>
+class Part1 {
+public:
+	T data;
+	std::vector<T> v;
+	int id = 0;
+	bool way;
+};
 template<class T, class U>
 class BinarySearchTree
 {
 public:
 	int d = 1;
-	U* root;
+	Node<T, U>* root;
 	/*! Constructer. Root = 0.*/
 	BinarySearchTree()
 	{
@@ -25,20 +45,19 @@ public:
 	/*!Creating a Part1.*/
 	void creat_leaf(T e, bool way1) {
 		U* cur = new U;
-		cur->data = e;
+		cur->data=e;
 		cur->way = way1;
-		cur->left = NULL;
-		cur->right = NULL;
 		cur->id = d++;
 		AddLeaf(cur, root);
 	};
 	/*! Adding a Part1.*/
-	void AddLeaf(U* cur, U* node) {
+	void AddLeaf(U* cur, Node<T, U>* node) {
 		if (root == NULL)
 		{
-			root = cur;
+			root = new Node<T, U>;
+			root->data = cur;
 		}
-		else if (cur->data <= node->data)
+		else if (cur->data <= node->data->data)
 		{
 			if (node->left != NULL)
 			{
@@ -46,11 +65,12 @@ public:
 			}
 			else
 			{
-				node->left = cur;
+				node->left =new Node<T, U>;
+				node->left->data = cur;
 				cur->way = 0;
 			}
 		}
-		else if (cur->data > node->data)
+		else if (cur->data > node->data->data)
 		{
 			if (node->right != NULL)
 			{
@@ -58,317 +78,300 @@ public:
 			}
 			else
 			{
-				node->right = cur;
+				node->right = new Node<T, U>;
+				node->right->data = cur;
 				cur->way = 1;
 			}
 		}
 	}
 	/*! Ствоюємо вузол для вектора.*/
-	void creat_leaf_v(std::vector<T> vv, bool way1) {
-		U* cur = new U;
-		cur->v = vv;
-		cur->way = way1;
-		cur->left = NULL;
-		cur->right = NULL;
-		cur->id = d++;
-		AddLeaf_v(cur, root);
-	};
+	//void creat_leaf_v(std::vector<T> vv, bool way1) {
+	//	U* cur = new U;
+	//	cur->v = vv;
+	//	cur->way = way1;
+	//	cur->left = NULL;
+	//	cur->right = NULL;
+	//	cur->id = d++;
+	//	AddLeaf_v(cur, root);
+	//};
 	/*! Deleting the Part1.*/
-	void RemoveNode(U* p, U* parent, int a)
-	{
-		if (root != NULL)
-		{
-			if (root == p)
-			{
-				RemoveRoot(a);
-			}
-			else
-			{
-				if (p->data <= parent->data && parent->left != NULL)
-				{
-					parent->left == p ?
-						RemoveMatch(parent, parent->left, true, a) :
-						RemoveNode(p, parent->left, a);
-				}
-				else if (p->data > parent->data && parent->right != NULL)
-				{
-					parent->right == p ?
-						RemoveMatch(parent, parent->right, false, a) :
-						RemoveNode(p, parent->right, a);
-				}
-			}
-		}
-	}
-	/*! Видалення вузла вектора.*/
-	void RemoveNode_vect(U* p, U* parent, int a)
-	{
-		if (root != NULL)
-		{
-			if (root == p)
-			{
-				RemoveRoot(a);
-			}
-			else
-			{
-				if (p->v[0] <= parent->data && parent->left != NULL)
-				{
-					parent->left == p ?
-						RemoveMatch_vect(parent, parent->left, true, a) :
-						RemoveNode_vect(p, parent->left, a);
-				}
-				else if (p->v[0] > parent->data && parent->right != NULL)
-				{
-					parent->right == p ?
-						RemoveMatch_vect(parent, parent->right, false, a) :
-						RemoveNode_vect(p, parent->right, a);
-				}
-			}
-		}
-	}
-private:
-
-	void AddLeaf_v(U* cur, U* node) {
-		if (root == NULL)
-		{
-			root = cur;
-		}
-		else if (cur->v[0] <= node->v[0])
-		{
-			if (node->left != NULL)
-			{
-				AddLeaf_v(cur, node->left);
-			}
-			else
-			{
-				node->left = cur;
-				cur->way = 0;
-			}
-		}
-		else if (cur->v[0] > node->v[0])
-		{
-			if (node->right != NULL)
-			{
-				AddLeaf_v(cur, node->right);
-			}
-			else
-			{
-				node->right = cur;
-				cur->way = 1;
-			}
-		}
-	}
-	U* Find_smallestPrivate(U* node)
-	{
-		if (node->left != NULL)
-		{
-			return Find_smallestPrivate(node->left);
-		}
-		else { return node; };
-	}
-	/*! Deleting the root.*/
-	void RemoveRoot(int a) {
-		U* delPtr = root;
-		if (root->left == NULL && root->right == NULL)
-		{
-			root = NULL;
-
-		}
-		else if (root->left == NULL && root->right != NULL)
-		{
-			root = root->right;
-			delPtr->right = NULL;
-
-		}
-		else if (root->left != NULL && root->right == NULL)
-		{
-			root = root->left;
-			delPtr->left = NULL;
-
-		}
-		else
-		{
-			U* p = Find_smallest();
-			RemoveNode(p, root, a);
-			root->data = p->data;
-		}
-	}
-	/*! Delete the node that was found before.*/
-	void RemoveMatch(U* parent, U* match, bool left, int a) {
-
-		if (match->left == NULL && match->right == NULL)
-		{
-			left == true ? parent->left = NULL : parent->right = NULL;
-		}
-		else if (match->left == NULL && match->right != NULL && a == 1)
-		{
-			left == true ? parent->left = match->right : parent->right = match->right;
-			match->right = NULL;
-		}
-		else if (match->left != NULL && match->right == NULL && a == 1)
-		{
-			left == true ? parent->left = match->left : parent->right = match->left;
-			match->left = NULL;
-		}
-		else if (a == 2)
-		{
-			if (parent->left == match) { parent->left = NULL; }
-			else(parent->right = NULL);
-		}
-		else
-		{
-			U* p = Find_smallestPrivate(match->right);
-			RemoveNode(p, match, a);
-			match->data = p->data;
-		}
-	}
-	void RemoveRoot_vect(int a) {
-		U* delPtr = root;
-		if (root->left == NULL && root->right == NULL)
-		{
-			root = NULL;
-
-		}
-		else if (root->left == NULL && root->right != NULL)
-		{
-			root = root->right;
-			delPtr->right = NULL;
-
-		}
-		else if (root->left != NULL && root->right == NULL)
-		{
-			root = root->left;
-			delPtr->left = NULL;
-
-		}
-		else
-		{
-			U* p = Find_smallest();
-			RemoveNode_vect(p, root);
-			root->v.resize(p->v.size());
-			for (int i = 0; i < root->v.size(); i++) {
-				root->v[i] = p->v[i];
-			}
-		}
-	}
-	void RemoveMatch_vect(U* parent, U* match, bool left, int a) {
-
-		if (match->left == NULL && match->right == NULL)
-		{
-			left == true ? parent->left = NULL : parent->right = NULL;
-		}
-		else if (match->left == NULL && match->right != NULL && a == 1)
-		{
-			left == true ? parent->left = match->right : parent->right = match->right;
-			match->right = NULL;
-		}
-		else if (match->left != NULL && match->right == NULL && a == 1)
-		{
-			left == true ? parent->left = match->left : parent->right = match->left;
-			match->left = NULL;
-		}
-		else if (a == 2) {
-			if (parent->left == match) { parent->left = NULL; }
-			else(parent->right = NULL);
-		}
-		else
-		{
-			U* p = Find_smallestPrivate(match->right);
-			RemoveNode(p, match, a);
-			match->v.resize(p->v.size());
-			for (int i = 0; i < root->v.size(); i++) {
-				match->v[i] = p->v[i];
-			}
-		}
-	}
+//	void RemoveNode(U* p, U* parent, int a)
+//	{
+//		if (root != NULL)
+//		{
+//			if (root == p)
+//			{
+//				RemoveRoot(a);
+//			}
+//			else
+//			{
+//				if (p->data <= parent->data && parent->left != NULL)
+//				{
+//					parent->left == p ?
+//						RemoveMatch(parent, parent->left, true, a) :
+//						RemoveNode(p, parent->left, a);
+//				}
+//				else if (p->data > parent->data && parent->right != NULL)
+//				{
+//					parent->right == p ?
+//						RemoveMatch(parent, parent->right, false, a) :
+//						RemoveNode(p, parent->right, a);
+//				}
+//			}
+//		}
+//	}
+//	/*! Видалення вузла вектора.*/
+//	void RemoveNode_vect(U* p, U* parent, int a)
+//	{
+//		if (root != NULL)
+//		{
+//			if (root == p)
+//			{
+//				RemoveRoot(a);
+//			}
+//			else
+//			{
+//				if (p->v[0] <= parent->data && parent->left != NULL)
+//				{
+//					parent->left == p ?
+//						RemoveMatch_vect(parent, parent->left, true, a) :
+//						RemoveNode_vect(p, parent->left, a);
+//				}
+//				else if (p->v[0] > parent->data && parent->right != NULL)
+//				{
+//					parent->right == p ?
+//						RemoveMatch_vect(parent, parent->right, false, a) :
+//						RemoveNode_vect(p, parent->right, a);
+//				}
+//			}
+//		}
+//	}
+//private:
+//
+//	void AddLeaf_v(U* cur, U* node) {
+//		if (root == NULL)
+//		{
+//			root = cur;
+//		}
+//		else if (cur->v[0] <= node->v[0])
+//		{
+//			if (node->left != NULL)
+//			{
+//				AddLeaf_v(cur, node->left);
+//			}
+//			else
+//			{
+//				node->left = cur;
+//				cur->way = 0;
+//			}
+//		}
+//		else if (cur->v[0] > node->v[0])
+//		{
+//			if (node->right != NULL)
+//			{
+//				AddLeaf_v(cur, node->right);
+//			}
+//			else
+//			{
+//				node->right = cur;
+//				cur->way = 1;
+//			}
+//		}
+//	}
+//	U* Find_smallestPrivate(U* node)
+//	{
+//		if (node->left != NULL)
+//		{
+//			return Find_smallestPrivate(node->left);
+//		}
+//		else { return node; };
+//	}
+//	/*! Deleting the root.*/
+//	void RemoveRoot(int a) {
+//		U* delPtr = root;
+//		if (root->left == NULL && root->right == NULL)
+//		{
+//			root = NULL;
+//
+//		}
+//		else if (root->left == NULL && root->right != NULL)
+//		{
+//			root = root->right;
+//			delPtr->right = NULL;
+//
+//		}
+//		else if (root->left != NULL && root->right == NULL)
+//		{
+//			root = root->left;
+//			delPtr->left = NULL;
+//
+//		}
+//		else
+//		{
+//			U* p = Find_smallest();
+//			RemoveNode(p, root, a);
+//			root->data = p->data;
+//		}
+//	}
+//	/*! Delete the node that was found before.*/
+//	void RemoveMatch(U* parent, U* match, bool left, int a) {
+//
+//		if (match->left == NULL && match->right == NULL)
+//		{
+//			left == true ? parent->left = NULL : parent->right = NULL;
+//		}
+//		else if (match->left == NULL && match->right != NULL && a == 1)
+//		{
+//			left == true ? parent->left = match->right : parent->right = match->right;
+//			match->right = NULL;
+//		}
+//		else if (match->left != NULL && match->right == NULL && a == 1)
+//		{
+//			left == true ? parent->left = match->left : parent->right = match->left;
+//			match->left = NULL;
+//		}
+//		else if (a == 2)
+//		{
+//			if (parent->left == match) { parent->left = NULL; }
+//			else(parent->right = NULL);
+//		}
+//		else
+//		{
+//			U* p = Find_smallestPrivate(match->right);
+//			RemoveNode(p, match, a);
+//			match->data = p->data;
+//		}
+//	}
+//	void RemoveRoot_vect(int a) {
+//		U* delPtr = root;
+//		if (root->left == NULL && root->right == NULL)
+//		{
+//			root = NULL;
+//
+//		}
+//		else if (root->left == NULL && root->right != NULL)
+//		{
+//			root = root->right;
+//			delPtr->right = NULL;
+//
+//		}
+//		else if (root->left != NULL && root->right == NULL)
+//		{
+//			root = root->left;
+//			delPtr->left = NULL;
+//
+//		}
+//		else
+//		{
+//			U* p = Find_smallest();
+//			RemoveNode_vect(p, root);
+//			root->v.resize(p->v.size());
+//			for (int i = 0; i < root->v.size(); i++) {
+//				root->v[i] = p->v[i];
+//			}
+//		}
+//	}
+//	void RemoveMatch_vect(U* parent, U* match, bool left, int a) {
+//
+//		if (match->left == NULL && match->right == NULL)
+//		{
+//			left == true ? parent->left = NULL : parent->right = NULL;
+//		}
+//		else if (match->left == NULL && match->right != NULL && a == 1)
+//		{
+//			left == true ? parent->left = match->right : parent->right = match->right;
+//			match->right = NULL;
+//		}
+//		else if (match->left != NULL && match->right == NULL && a == 1)
+//		{
+//			left == true ? parent->left = match->left : parent->right = match->left;
+//			match->left = NULL;
+//		}
+//		else if (a == 2) {
+//			if (parent->left == match) { parent->left = NULL; }
+//			else(parent->right = NULL);
+//		}
+//		else
+//		{
+//			U* p = Find_smallestPrivate(match->right);
+//			RemoveNode(p, match, a);
+//			match->v.resize(p->v.size());
+//			for (int i = 0; i < root->v.size(); i++) {
+//				match->v[i] = p->v[i];
+//			}
+//		}
+//	}
 };
-template< class T, class U>
-class Node {
-	friend class BinarySearchTree<T, Node<T, U>>;
-public:
-	Node<T, U>* left;
-	Node<T, U>* right;
-	U data;
-	/*std::vector<T> v;
-	int id = 0;
-	bool way;*/
-};
-template<class T>
-class Part1 {
-public:
-	T data;
-	std::vector<T> v;
-	int id = 0;
-	bool way;
-};
+
 template <class T, class U>
-class Functions : public BinarySearchTree<T, U>
+class Functions : public BinarySearchTree<T, U>, Node<T, U>
 {
 public:
 	friend class Node<T, U>;
 	/*! Search by the way.*/
-	void FindByTheWay(int a) {
-		bool count = false, i = 0;
-		int n;
-		cout << "Введите длину маршрута" << endl;
-		cin >> n;
-		bool* mas = new bool[n];
-		cout << "Заполните маршрут" << endl;
-		for (int j = 0; j < n; j++) {
-			cin >> mas[j];
-		}
-		if (a == 1) {
-			if (FindByTheWayPrivate1(mas, BinarySearchTree<T, U>::root, count, i, n) == 0)
-			{
-				cout << "Такого нема";
-			}
-		}
-		
-		delete[]mas;
-		system("pause");
-	}
-	/*! All ways to delete.*/
-	void Delete(int a, int b)
-	{
-		if (b == 1) {
-			T inf;
-			cout << "Введите, что хотите удалить" << endl;
-			cin >> inf;
-			if (a == 1)
-			{
-				U* ptr = FindPrivate1(inf, BinarySearchTree<T, U>::root);
-				if (ptr == NULL) { cout << "Такого нема" << endl; }
-				else  (BinarySearchTree<T, U>::RemoveNode(ptr, BinarySearchTree<T, U>::root, 1));
-			}
-			
-		}
-		else if (b == 2) {
-			PrintPreOrder(a);
-			int id;
-			cout << "Введите id" << endl;
-			cin >> id;
-			if (a == 1)
-			{
-				U* ptr = FindPrivateId1(id, BinarySearchTree<T, U>::root);
-				if (ptr == NULL) { cout << "Такого нема" << endl; }
-				else  (BinarySearchTree<T, U>::RemoveNode(ptr, BinarySearchTree<T, U>::root, 1));
-			}
-			
-		}
-		else if (b == 3)
-		{
-			PrintPreOrder(a);
-			int id;
-			cout << "Введите id отца" << endl;
-			cin >> id;
-			if (a == 1)
-			{
-				U* ptr = FindPrivateId1(id, BinarySearchTree<T, U>::root);
-				if (ptr == NULL) { cout << "Такого нема" << endl; }
-				else  (BinarySearchTree<T, U>::RemoveNode(ptr, BinarySearchTree<T, U>::root, 2));
-			}
-			
-		}
-	}
+	//void FindByTheWay(int a) {
+	//	bool count = false, i = 0;
+	//	int n;
+	//	cout << "Введите длину маршрута" << endl;
+	//	cin >> n;
+	//	bool* mas = new bool[n];
+	//	cout << "Заполните маршрут" << endl;
+	//	for (int j = 0; j < n; j++) {
+	//		cin >> mas[j];
+	//	}
+	//	if (a == 1) {
+	//		if (FindByTheWayPrivate1(mas, BinarySearchTree<T, U>::root, count, i, n) == 0)
+	//		{
+	//			cout << "Такого нема";
+	//		}
+	//	}
+	//	
+	//	delete[]mas;
+	//	system("pause");
+	//}
+	///*! All ways to delete.*/
+	//void Delete(int a, int b)
+	//{
+	//	if (b == 1) {
+	//		T inf;
+	//		cout << "Введите, что хотите удалить" << endl;
+	//		cin >> inf;
+	//		if (a == 1)
+	//		{
+	//			U* ptr = FindPrivate1(inf, BinarySearchTree<T, U>::root);
+	//			if (ptr == NULL) { cout << "Такого нема" << endl; }
+	//			else  (BinarySearchTree<T, U>::RemoveNode(ptr, BinarySearchTree<T, U>::root, 1));
+	//		}
+	//		
+	//	}
+	//	else if (b == 2) {
+	//		PrintPreOrder(a);
+	//		int id;
+	//		cout << "Введите id" << endl;
+	//		cin >> id;
+	//		if (a == 1)
+	//		{
+	//			U* ptr = FindPrivateId1(id, BinarySearchTree<T, U>::root);
+	//			if (ptr == NULL) { cout << "Такого нема" << endl; }
+	//			else  (BinarySearchTree<T, U>::RemoveNode(ptr, BinarySearchTree<T, U>::root, 1));
+	//		}
+	//		
+	//	}
+	//	else if (b == 3)
+	//	{
+	//		PrintPreOrder(a);
+	//		int id;
+	//		cout << "Введите id отца" << endl;
+	//		cin >> id;
+	//		if (a == 1)
+	//		{
+	//			U* ptr = FindPrivateId1(id, BinarySearchTree<T, U>::root);
+	//			if (ptr == NULL) { cout << "Такого нема" << endl; }
+	//			else  (BinarySearchTree<T, U>::RemoveNode(ptr, BinarySearchTree<T, U>::root, 2));
+	//		}
+	//		
+	//	}
+	//}
 	/*! Adding a node.*/
 	void AddLeaf(int a) {
 		T inf;
@@ -377,39 +380,39 @@ public:
 		if (a == 1) { BinarySearchTree<T, U>::creat_leaf(inf, 0); }
 	}
 	/*! Search by the information.*/
-	void Find(int a) {
-		T inf;
-		cout << "Введите, что хотите найти" << endl;
-		cin >> inf;
-		if (a == 1)
-		{
-			U* ptr = FindPrivate1(inf, BinarySearchTree<T, U>::root);
-			if (ptr == NULL) { cout << "Такого нема" << endl; }
-			else(cout << ptr->data->id << ". " << ptr->data << endl);
-			system("pause");
-		}
-	}
+	//void Find(int a) {
+	//	T inf;
+	//	cout << "Введите, что хотите найти" << endl;
+	//	cin >> inf;
+	//	if (a == 1)
+	//	{
+	//		Node* ptr = FindPrivate1(inf, BinarySearchTree<T, U>::root);
+	//		if (ptr == NULL) { cout << "Такого нема" << endl; }
+	//		else(cout << ptr->data.id << ". " << ptr->data << endl);
+	//		system("pause");
+	//	}
+	//}
 	/*! Print.*/
 	void PrintInOrder(int a) {
 		if (a == 1) { PrintInOrderPrivate1(BinarySearchTree<T, U>::root); }
 		system("pause");
 	}
-	/*! Print.*/
-	/*! Print.*/
-	void PrintPostOrder(int a) {
-		if (a == 1) { PrintPostOrderPrivate1(BinarySearchTree<T, U>::root); }
-		system("pause");
-	}
-	/*! Print.*/
-	/*! Print.*/
-	void PrintPreOrder(int a) {
-		if (a == 1) { PrintPreOrderPrivate1(BinarySearchTree<T, U>::root); }
-		system("pause");
-	}
-	/*! Print.*/
+	///*! Print.*/
+	///*! Print.*/
+	//void PrintPostOrder(int a) {
+	//	if (a == 1) { PrintPostOrderPrivate1(BinarySearchTree<T, U>::root); }
+	//	system("pause");
+	//}
+	///*! Print.*/
+	///*! Print.*/
+	//void PrintPreOrder(int a) {
+	//	if (a == 1) { PrintPreOrderPrivate1(BinarySearchTree<T, U>::root); }
+	//	system("pause");
+	//}
+	///*! Print.*/
 
 private:
-	bool FindByTheWayPrivate1(bool* mas, U* node, bool count, int i, int n) {
+	/*bool FindByTheWayPrivate1(bool* mas, U* node, bool count, int i, int n) {
 		if (node != NULL)
 		{
 			if (i == n)
@@ -432,8 +435,8 @@ private:
 			}
 		}
 		else { return count; }
-	}
-	void PrintInOrderPrivate1(U* node)
+	}*/
+	void PrintInOrderPrivate1(Node<T,U>* node)
 	{
 		if (BinarySearchTree<T, U>::root != NULL)
 		{
@@ -441,8 +444,8 @@ private:
 			{
 				PrintInOrderPrivate1(node->left);
 			}
-			cout << node->id << ". ";
-			cout << node->data << endl;
+			cout << node->data->id << ". ";
+			cout << node->data->data << endl;
 			if (node->right != NULL)
 			{
 				PrintInOrderPrivate1(node->right);
@@ -450,7 +453,7 @@ private:
 		}
 		else(cout << "Пусто" << endl);
 	}
-	void PrintPreOrderPrivate1(U* node) {
+	/*void PrintPreOrderPrivate1(U* node) {
 		if (node != NULL)
 		{
 			cout << node->id << ".";
@@ -483,7 +486,7 @@ private:
 		}
 		else(cout << "Пусто" << endl);
 	}
-	U* FindPrivate1(T e, U* node)
+	Node* FindPrivate1(T e, U* node)
 	{
 		if (node != NULL)
 		{
@@ -518,7 +521,7 @@ private:
 		if (tmp2 != NULL) {
 			if (id == tmp2->id) { return tmp2; }
 		}
-	}
+	}*/
 };
 
 int menu_console() {
@@ -578,8 +581,8 @@ int menu_console() {
 }
 int menu1(bool isRunning, int a)
 {
-	Functions<int, Node<int, Part1<int>>> i;
-	Functions<int, Node<int, Part1<int>>> i_v;
+	Functions<int, Part1<int>> i;/*
+	Functions<int, Node<int, Part1<int>>> i_v;*/
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 	isRunning = true;
@@ -592,7 +595,7 @@ int menu1(bool isRunning, int a)
 		{
 		case 0: i.AddLeaf(a); break;
 		case 1: i.PrintInOrder(a); break;
-		case 2: i.PrintPreOrder(a); break;
+	/*	case 2: i.PrintPreOrder(a); break;
 		case 3: i.PrintPostOrder(a); break;
 		case 4: i.FindByTheWay(a); break;
 		case 5: i.Find(a); break;
@@ -606,7 +609,7 @@ int menu1(bool isRunning, int a)
 		case 13: i_v.FindByTheWay(a); break;
 		case 14: ; break;
 		case 15: ; break;
-		case 16:; break;
+		case 16:; break;*/
 		case 17: ; break;
 		case 18: system("cls"); cout << "Goodbye!\n__________________"; isRunning = false;
 		}
@@ -638,7 +641,7 @@ int menu_inf(int a) {
 	system("cls");
 	return key;
 }
-int inf(bool isRunning, int a)
+int main(bool isRunning, int a)
 {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
@@ -649,7 +652,7 @@ int inf(bool isRunning, int a)
 		answer = menu_inf(a);
 		switch (answer)
 		{
-		case 0: menu1(true, a); break;
+		case 0: menu1(true, 1); break;
 		case 4: system("cls"); cout << "Goodbye!\n__________________"; isRunning = false;
 		}
 
