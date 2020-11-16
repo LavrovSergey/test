028d80dcc50329c30fd564575707aece682bd381
LavrovSergey <54863977+LavrovSergey@users.noreply.github.com>
2020-11-16 02:16:31 +0200

    nkbj

--- Book.h
+++ Book.h
@@ -4,7 +4,7 @@
 
 /*! Book class.*/
 class Book {
-public:
+private:
 	string data; //name of the book
 	vector<std::string> authors;
 	int v;
@@ -12,5 +12,29 @@ public:
 	int pages;
 	string plot;
 	int id = 0;
+public:
+	Book(string data, std::vector<string> authors, int v, int d, int m, int y,int pages,string plot,int id,bool way) {
+
+		this->data = data;
+		this->authors = authors;
+		this->v = v;
+		this->d = d;
+		this->m = m;
+		this->y = y;
+		this->pages =pages;
+		this->plot = plot;
+		this->id= id;
+		this->way = way;
+	};
+	string GetData() { return data; };
+	string GetAuthor(int i) { return authors[i]; };
+	int GetDay() { return d; };
+	int GetDataMonth() { return m; };
+	int GetYear() { return y; };
+	int GetPages() { return pages; };
+	string GetPlot() { return plot; };
+	/*int GetVectorSize() { return v.size(); };*/
+	/*std::vector<T> GetVectorF() { return v; };*/
+	int GetId() { return id; };
 	bool way;
 };
--- Hero.h
+++ Hero.h
@@ -4,12 +4,24 @@
 
 /*! Character class.*/
 class Hero {
-public:
+private:
 	string data; //name of the hero
 	int v;
 	vector<string> names;
-	int m;
-	multimap<int, string> hb;
 	int id = 0;
+public:
+	Hero(string data,int v, vector<string> names, int id) {
+
+		this->data = data;
+		this->names = names;
+		this->v = v;
+		this->id = id;
+		this->way = way;
+	};
+	string GetData() { return data; };
+	string GetName(int i) { return names[i]; };
+	/*int GetVectorSize() { return v.size(); };*/
+	/*std::vector<T> GetVectorF() { return v; };*/
+	int GetId() { return id; };
 	bool way;
 };
